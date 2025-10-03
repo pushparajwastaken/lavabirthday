e2cbe284f56fff5c3b8d3e44ee74b7f4c29277df
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-10-03 11:27:40 +0530

    basic work started

--- lavaaa/src/App.jsx
+++ lavaaa/src/App.jsx
@@ -1,1 +1,14 @@
-
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+export default App;
--- lavaaa/src/main.jsx
+++ lavaaa/src/main.jsx
@@ -4,6 +4,6 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Navbar />
+    <App />
   </StrictMode>
 );
